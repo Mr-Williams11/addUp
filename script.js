b86fcf28684b3eb6e5a5201118e2da022422c89f
Mr-Williams11 <joelwilliams742@gmail.com>
2023-11-22 15:10:19 +0200

    finished

---
+++ script.js
@@ -0,0 +1,18 @@
+    function calculateSum(...numbers) {
+    if (numbers.length === 0) {
+      return 'Provide only numbers';
+    }
+    let sum = numbers.reduce((acc, num) => {
+      return acc + (typeof num === 'number' ? num : 0);
+    }, 0);
+
+    return sum;
+  }
+  document.getElementById('calculateButton').addEventListener('click', function() {
+    displaySum(calculateSum(41, 65, 23, 48, 'five'));
+  });
+
+  function displaySum(sum) {
+    let resultElement = document.getElementById('result');
+    resultElement.innerHTML = typeof sum === 'number' ? 'Sum: ' + sum : sum;
+  }
